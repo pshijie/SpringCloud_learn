8544a17028dc6630385a0030c935f8a214b9afae
psj <729589165@qq.com>
2023-03-30 21:42:55 +0800

    SpringCloud_day01代码

---
+++ day01/cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,31 @@
+package cn.itcast.order;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    // 之后无论orderservice调用哪个服务模块都使用该规则
+//    @Bean
+//    public IRule randomRule() {
+//        return new RandomRule();
+//    }
+}
